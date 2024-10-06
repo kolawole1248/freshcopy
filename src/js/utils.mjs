49f1a93bb72f4cfc756ff9ed6ecbaf17a954e049
ibrahimtp <oyetunjibrahimibrahim@gmail.com>
2024-10-06 10:07:54 -0700

    Expand Inventory

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,10 +32,49 @@ export function getParam(param) {
   return product;
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(templateFn); 
+// export function renderWithTemplate(template, parentElement, data, callback) {
+//   parentElement.insertAdjacentHTML("afterbegin", template);
+//   //if there is a callback...call it and pass data
+//   if (callback) {
+//     callback(data);
+//   }
+// }
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
   if (clear) {
-    parentElement.clear;
-  } 
+    parentElement.innerHTML = "";
+  }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  //if there is a callback...call it and pass data
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  // renderWithTemplate(headerTemplate, headerElement);
+  // renderWithTemplate(footerTemplate, footerElement);
 }
